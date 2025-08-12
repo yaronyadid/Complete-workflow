7aaec7a91334943cb9b5261e3e413cb966457f97
Yaron Yadid <yyadid7@gmail.com>
2025-08-12 23:44:15 +0300

    adding testing for the app

---
+++ my-app/test-app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello from my App" in response.data
